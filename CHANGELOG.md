28346dbd0f3691d46557292864a544798c7f61ba
MikeTeddyOmondi <mike_omondi@outlook.com>
2024-07-29 02:39:03 +0300

    Add: .dockerignore, .justfile, CHANGELOG.md, Dockerfile, compose.yaml

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,6 @@
 These are the proposed changes as follows:
   - Add `Dockerfile`, `.dockerignore` and `compose.yaml` files
   - Add `.justfile` for productivity boost
-  - _Fix_ - tab spaces
   - Add `CHANGELOG.md` for documenting changes
+  - _Fix_ - tab spaces
   
--- src/components/Color.jsx
+++ src/components/Color.jsx
@@ -4,40 +4,40 @@ import { NotesContext } from "../context/NotesContext";
 import { db } from "../appwrite/databases";
 
 const Color = ({ color }) => {
-    const { selectedNote, notes, setNotes } = useContext(NotesContext);
+  const { selectedNote, notes, setNotes } = useContext(NotesContext);
 
-    const changeColor = () => {
-        console.log("Selected color:", selectedNote);
+  const changeColor = () => {
+    console.log("Selected color:", selectedNote);
 
-        try {
-            const currentNoteIndex = notes.findIndex(
-                (note) => note.$id === selectedNote.$id
-            );
+    try {
+      const currentNoteIndex = notes.findIndex(
+        (note) => note.$id === selectedNote.$id
+      );
 
-            const updatedNote = {
-                ...notes[currentNoteIndex],
-                colors: JSON.stringify(color),
-            };
+      const updatedNote = {
+        ...notes[currentNoteIndex],
+        colors: JSON.stringify(color),
+      };
 
-            const newNotes = [...notes];
-            newNotes[currentNoteIndex] = updatedNote;
-            setNotes(newNotes);
+      const newNotes = [...notes];
+      newNotes[currentNoteIndex] = updatedNote;
+      setNotes(newNotes);
 
-            db.notes.update(selectedNote.$id, {
-                colors: JSON.stringify(color),
-            });
-        } catch (error) {
-            alert("You must select a note before changing colors");
-        }
-    };
+      db.notes.update(selectedNote.$id, {
+        colors: JSON.stringify(color),
+      });
+    } catch (error) {
+      alert("You must select a note before changing colors");
+    }
+  };
 
-    return (
-        <div
-            onClick={changeColor}
-            className="color"
-            style={{ backgroundColor: color.colorHeader }}
-        ></div>
-    );
+  return (
+    <div
+      onClick={changeColor}
+      className="color"
+      style={{ backgroundColor: color.colorHeader }}
+    ></div>
+  );
 };
 
 export default Color;
